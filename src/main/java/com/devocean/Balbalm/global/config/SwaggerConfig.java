24ab688339dd6a193c11bcaa26bc496fbf7ce631
tmddus2 <tmddus2__@naver.com>
2024-11-19 17:02:55 +0900

    Feat: Swagger 인증 추가

---
+++ src/main/java/com/devocean/Balbalm/global/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.devocean.Balbalm.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI customOpenAPI() {
+		SecurityScheme securityScheme = new SecurityScheme()
+			.name("Authorization")
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT");
+		SecurityRequirement securityRequirement = new SecurityRequirement()
+			.addList("Authorization");
+		return new OpenAPI()
+			.components(new Components().addSecuritySchemes("Authorization", securityScheme))
+			.addSecurityItem(securityRequirement)
+			.info(new Info()
+				.title("Balbalm API")
+				.description("Balbalm 프로젝트의 API 문서입니다.")
+				.version("1.0.0"));
+	}
+}
